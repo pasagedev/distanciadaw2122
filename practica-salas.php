87a9be2f07e936458d7d17658700b4778e520658
Pablo Salas Gelich <pablosalas@outlook.com.ar>
2022-05-24 19:21:01 +0200

    primer commit

---
+++ practica-salas.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+  *  @author Pablo Salas Gelich
+  *  @version 1.0
+*/
+
+/**
+ * Función que suma dos números
+ *
+ * @param float $num1 Primer valor a sumar
+ * @param float $num2 Segundo valor a sumar
+ * @return float Valor con el resultado de la suma
+ */
+function sumar($num1, $num2){
+    return $num1 + $num2;
+}
+
+/**
+ * Función que devuelve un saludo con el nombre pasado por argumento
+ *
+ * @internal
+ * @param string $nombre nombre que contendrá el saludo
+ * @return string Cadena con el nombre y saludo.
+ *
+ */
+function saludar($nombre){
+    return "Hola $nombre!, cómo estás?";
+}
+
+echo sumar(10, 10);
+echo saludar("Ana");
+?>
